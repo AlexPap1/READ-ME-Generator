e1cf3ef65ce7a7103c5c54bd5a326d383569f894
AlexPap <arpappagallo@gmail.com>
2022-04-29 16:46:04 -0400

    start challenge 9. Setup Inquirer and starter code

---
+++ index.js
@@ -0,0 +1,16 @@
+// List of Dependencies. Include packages needed for this application:
+const generateMarkdown = require("./utils/generateMarkdown");
+const fs = require("fs");
+const inquirer = require("inquirer");
+
+// TODO: Create an array of questions for user input
+const questions = [];
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {}
+
+// TODO: Create a function to initialize app
+function init() {}
+
+// Function call to initialize app
+init();
